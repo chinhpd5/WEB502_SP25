5bf5804d89713033cc8b0ad1c01e7402bd046558
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-06 17:53:22 +0700

    string, number, boolean

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,23 @@
 "use strict";
-console.log("xin chào các bạn");
-console.log("chinhpd5");
+// tsc -init tạo tsconfig.json
+// sửa nội dung trong tsconfig
+// tạo file src/index.ts và nhập nội dung
+// tạo file index.html và link dist/index.js bằng thẻ script
+// tsc -w
+// string
+let myName = "chinhpd5";
+let myHome = 'Hà Nội';
+let greeting = `Chào mừng bạn ${myName}`;
+// console.log(greeting.length);
+// number
+let myAge = 20;
+let price = 1.222;
+// console.log(price.toFixed(2));
+// boolean
+let myGender = true;
+if (myGender) {
+    console.log("Nam");
+}
+else {
+    console.log('Nữ');
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
 // tsc -init tạo tsconfig.json
 // sửa nội dung trong tsconfig
+// tạo file src/index.ts và nhập nội dung
+// tạo file index.html và link dist/index.js bằng thẻ script
 // tsc -w
 
-console.log("xin chào các bạn");
+// string
+let myName: string = "chinhpd5";
+let myHome: string = 'Hà Nội';
+let greeting: string = `Chào mừng bạn ${myName}`
 
-console.log("chinhpd5");
+// console.log(greeting.length);
+
+// number
+let myAge: number = 20;
+let price: number = 1.222;
+
+// console.log(price.toFixed(2));
+
+// boolean
+let myGender: boolean= true;
+
+if(myGender){
+    console.log("Nam");
+    
+}else{
+    console.log('Nữ');
+    
+}
+
+
+
+
+
+
 
