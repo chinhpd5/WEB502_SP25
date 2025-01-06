f940d38634bb8ace0740951836b90b13d1628f0d
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-06 19:55:28 +0700

    function, bài tập

--- dist/index.js
+++ dist/index.js
@@ -16,9 +16,26 @@ let price = 2.44444;
 // console.log(price.toFixed(2));
 //boolean
 let myGender = true;
-if (myGender) {
-    console.log('Nam');
+// if(myGender){
+//     console.log('Nam');
+// }else{
+//     console.log('Nữ');
+// }
+function sayHello(name) {
+    return `Xin chào ${name}`;
 }
-else {
-    console.log('Nữ');
-}
+// console.log(sayHello("chinhpd5"));
+let sayHello2 = function (age) {
+    console.log(`Tuổi của bạn ${age}`);
+};
+// sayHello2(20)
+let sayHello3 = (home) => {
+    return `Quê quán: ${home}`;
+};
+// console.log(sayHello3("Hà Nội"));
+/**
+ * 1. Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
+ * 2. Viết 1 hàm tính tổng nhiều số (không biết trước số lượng tham số), sử dụng rest parameter
+ * 3. Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
+ * 4. Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
+ */
--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,38 @@ let price:number = 2.44444;
 //boolean
 let myGender: boolean = true;
 
-if(myGender){
-    console.log('Nam');
-}else{
-    console.log('Nữ');
-    
+// if(myGender){
+//     console.log('Nam');
+// }else{
+//     console.log('Nữ');
+// }
+
+function sayHello(name: string): string {
+    return `Xin chào ${name}`
 }
+
+// console.log(sayHello("chinhpd5"));
+
+let sayHello2 = function(age: number): void {
+    console.log(`Tuổi của bạn ${age}`);
+}
+
+// sayHello2(20)
+
+let sayHello3 = (home: string):string => {
+    return `Quê quán: ${home}`
+}
+
+// console.log(sayHello3("Hà Nội"));
+
+/**
+ * 1. Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
+ * 2. Viết 1 hàm tính tổng nhiều số (không biết trước số lượng tham số), sử dụng rest parameter
+ * 3. Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
+ * 4. Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
+ */
+
+
+
 
 
