9ceaf7899ac10d4b22ad4569d7b60618b4169e83
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-06 17:30:23 +0700

    config

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("xin chào các bạn");
+console.log("chinhpd5");
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// tsc -init tạo tsconfig.json
+// sửa nội dung trong tsconfig
+// tsc -w
+
+console.log("xin chào các bạn");
+
+console.log("chinhpd5");
+
