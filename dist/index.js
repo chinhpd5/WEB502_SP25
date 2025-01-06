5c4831b9793ea2e9a1aff680ae9d8ed1666e1fe0
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-06 19:28:24 +0700

    config

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// tsc -init tạo tsconfig
+// thay đổi nội dung 
+// tạo file src/index.ts
+// tsc -w
+// tạo file index.html
+// link dist/index.js vào index.html
+console.log("chinhpd5");
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// tsc -init tạo tsconfig
+// thay đổi nội dung 
+// tạo file src/index.ts
+// tsc -w
+// tạo file index.html
+// link dist/index.js vào index.html
+
+console.log("chinhpd5");
