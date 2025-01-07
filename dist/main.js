6f349cb38078e25ec2b898d5a3de21037d1178dc
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-07 08:28:59 +0700

    string, number, boolean

--- dist/main.js
+++ dist/main.js
@@ -5,5 +5,20 @@
 // biên dịch: tsc --watch hoặc tsc -w
 // tạo file index.html
 // liên kết dist/main.js vào index.html
-console.log("Xin chào các bạn");
-console.log("chinhpd5");
+// string
+let myName = "chinhpd5";
+let myHome = 'Hà Nội';
+let greeting = `Xin chào ${myName}`;
+// console.log(greeting.toUpperCase());
+// number
+let myAge = 20;
+let price = 2.12345;
+// console.log(price.toFixed(3));
+// boolean
+let myGender = true;
+if (myGender) {
+    console.log(`Giới tính Nam`);
+}
+else {
+    console.log(`Giới tính Nữ`);
+}
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,27 @@
 // tạo file index.html
 // liên kết dist/main.js vào index.html
 
-console.log("Xin chào các bạn");
+// string
+let myName: string = "chinhpd5";
+let myHome: string = 'Hà Nội';
+let greeting: string = `Xin chào ${myName}`
 
-console.log("chinhpd5");
+// console.log(greeting.toUpperCase());
+
+// number
+let myAge: number = 20;
+let price: number = 2.12345;
+
+// console.log(price.toFixed(3));
+
+// boolean
+let myGender: boolean = true;
+
+if(myGender){
+    console.log(`Giới tính Nam`);
+}else{
+    console.log(`Giới tính Nữ`);
+}
+
+
 
