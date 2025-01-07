f55a69018a3390dfd25595a05d3453b96e6f3c9e
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-07 08:43:34 +0700

    function, bài tập

--- dist/main.js
+++ dist/main.js
@@ -16,9 +16,27 @@ let price = 2.12345;
 // console.log(price.toFixed(3));
 // boolean
 let myGender = true;
-if (myGender) {
-    console.log(`Giới tính Nam`);
+// if(myGender){
+//     console.log(`Giới tính Nam`);
+// }else{
+//     console.log(`Giới tính Nữ`);
+// }
+// function 
+function sayHello(name) {
+    return `Chào mừng bạn ${name}`;
 }
-else {
-    console.log(`Giới tính Nữ`);
-}
+// console.log(sayHello("chinhpd5"));
+let sayHello2 = function (age) {
+    console.log(`Tuổi của bạn: ${age}`);
+};
+// sayHello2(20)
+let sayHello3 = (home) => {
+    return `Quê quán: ${home}`;
+};
+// console.log(sayHello3("Hà Nam"));
+/**
+ * 1. Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
+ * 2. Viết 1 hàm tính tổng nhiều số (không biết trước số lượng tham số), sử dụng rest parameter
+ * 3. Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
+ * 4. Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
+ */
--- src/main.ts
+++ src/main.ts
@@ -21,11 +21,40 @@ let price: number = 2.12345;
 // boolean
 let myGender: boolean = true;
 
-if(myGender){
-    console.log(`Giới tính Nam`);
-}else{
-    console.log(`Giới tính Nữ`);
+// if(myGender){
+//     console.log(`Giới tính Nam`);
+// }else{
+//     console.log(`Giới tính Nữ`);
+// }
+
+// function 
+
+function sayHello(name: string): string{
+    return `Chào mừng bạn ${name}`
 }
+
+// console.log(sayHello("chinhpd5"));
+
+let sayHello2 = function(age: number): void{
+    console.log(`Tuổi của bạn: ${age}`);
+}
+
+// sayHello2(20)
+
+let sayHello3 = (home:string): string =>{
+    return `Quê quán: ${home}`
+}
+
+// console.log(sayHello3("Hà Nam"));
+
+/**
+ * 1. Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
+ * 2. Viết 1 hàm tính tổng nhiều số (không biết trước số lượng tham số), sử dụng rest parameter
+ * 3. Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
+ * 4. Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
+ */
+
+
 
 
 
