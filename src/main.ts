38ba49e22286ba62ba7aa81f672bc4422d914cfd
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-07 08:10:25 +0700

    config

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+// tsc -init tạo file tsconfig
+// thay đổi nội dung của file config
+// tạo file src/main.ts và gõ nội dung
+// biên dịch: tsc --watch hoặc tsc -w
+// tạo file index.html
+// liên kết dist/main.js vào index.html
+
+console.log("Xin chào các bạn");
+
+console.log("chinhpd5");
+
