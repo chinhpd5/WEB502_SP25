53f64fb53e94aa843cdc73a185c2d5521e0c9ae6
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-08 20:40:21 +0700

    buổi 2

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@
 // link dist/index.js vào index.html
 //string
 let myName = "Chinhpd5";
+// myName = 30
 let myHome = 'Hà Nội';
 let greeting = `Chào mừng bạn ${myName}`;
 // console.log(greeting.toLowerCase());
@@ -33,9 +34,51 @@ let sayHello3 = (home) => {
     return `Quê quán: ${home}`;
 };
 // console.log(sayHello3("Hà Nội"));
+// let variable = 30
 /**
  * 1. Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
  * 2. Viết 1 hàm tính tổng nhiều số (không biết trước số lượng tham số), sử dụng rest parameter
  * 3. Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
  * 4. Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
  */
+// Câu 1:
+function question1(a, b) {
+    return `Chu vi: ${(a + b) * 2}, Diện tích: ${a * b}`;
+}
+// console.log(question1(3,4));
+// Câu 2:
+// spread
+// rest
+function sum(...rest) {
+    // console.log(rest);
+    let total = 0;
+    rest.forEach((item) => {
+        // console.log(item);
+        total += item;
+    });
+    return total;
+}
+// console.log(sum(1,2,3,4,5));
+// console.log(sum(1,2,3,4,5,6,7,8,9));
+// Câu 3
+function countLetter(text, letter) {
+    let count = 0;
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == letter)
+            count++;
+    }
+    return count;
+}
+// console.log(countLetter('xin chao cac ban', 'n'));
+function nguyenTo(num) {
+    if (num <= 0)
+        return false;
+    if (num <= 2)
+        return true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+// console.log(nguyenTo(9));
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@
 
 //string
 let myName: string = "Chinhpd5";
+// myName = 30
 let myHome: string = 'Hà Nội';
 let greeting: string = `Chào mừng bạn ${myName}`;
 
@@ -14,7 +15,7 @@ let greeting: string = `Chào mừng bạn ${myName}`;
 
 //number
 let myAge: number = 30;
-let price:number = 2.44444;
+let price: number = 2.44444;
 
 // console.log(price.toFixed(2));
 
@@ -44,6 +45,7 @@ let sayHello3 = (home: string):string => {
 }
 
 // console.log(sayHello3("Hà Nội"));
+// let variable = 30
 
 /**
  * 1. Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
@@ -51,6 +53,63 @@ let sayHello3 = (home: string):string => {
  * 3. Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
  * 4. Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
  */
+
+// Câu 1:
+function question1(a: number, b: number): string{
+    return `Chu vi: ${(a+b)*2}, Diện tích: ${a*b}`
+}
+
+// console.log(question1(3,4));
+
+// Câu 2:
+// spread
+// rest
+function sum(...rest: number[]): number {
+    // console.log(rest);
+
+    let total: number = 0;
+    rest.forEach((item: number)=>{
+        // console.log(item);
+        total += item;
+    })
+    return total;
+}
+
+// console.log(sum(1,2,3,4,5));
+// console.log(sum(1,2,3,4,5,6,7,8,9));
+
+
+// Câu 3
+function countLetter(text: string, letter: string): number {
+    let count: number = 0;
+
+    for(let i: number = 0; i < text.length; i++){
+        if(text[i] == letter)
+            count++;
+    }
+
+    return count;
+}
+
+// console.log(countLetter('xin chao cac ban', 'n'));
+
+function nguyenTo(num: number): boolean {
+    if(num <= 0 )
+        return false
+    if(num <=2)
+        return true
+    for(let i: number = 2; i <= Math.sqrt(num); i++){
+        if(num % i == 0)
+            return false;
+    }
+    return true
+}
+
+// console.log(nguyenTo(9));
+
+
+
+
 
 
 
