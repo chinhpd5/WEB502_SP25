90bceaa810c7b92403d3a892377d9c8642ba9305
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-12 17:31:17 +0700

    eslint

--- wd19319/src/App.tsx
+++ wd19319/src/App.tsx
@@ -2,6 +2,15 @@
 import MyComponent from './components/MyComponent'
 
 function App() {
+    // const myName:string = "chinhpd5";
+
+    // console.log(123);
+
+    function test(a:number, b:number):void{
+        // debugger;
+        console.log(a+b);
+    }
+    test(1,2);
 
   return (
     <>
