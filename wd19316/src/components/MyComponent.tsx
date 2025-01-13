bcfd9fcb1c8bc20ec4d8151b038966cc67844fb3
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-13 19:28:27 +0700

    event

--- wd19316/src/components/MyComponent.tsx
+++ wd19316/src/components/MyComponent.tsx
@@ -7,34 +7,57 @@ type ProductType = {
 //Props
 
 function ProductItem(props: ProductType){
-  console.log(props);
-
   return (
     <>
       <h1 id="title">{props.title}</h1>
       <img className="" style={{height: '200px'}} src={props.urlImage} alt="" />
       <h3>{props.price} vnđ</h3>
-      <button>Mua ngay</button>
+      <button onClick={()=>{
+        console.log(props.title);
+      }}>Mua ngay</button>
     </>
   )
 }
 
 function MyComponent(){
+  const listProduct: ProductType[] = [
+    {
+      title: "sản phẩm 1",
+      urlImage: "https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg",
+      price: 100
+    },
+    {
+      title: "sản phẩm 2",
+      urlImage: "https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg",
+      price: 200
+    },
+    {
+      title: "sản phẩm 3",
+      urlImage: "https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg",
+      price: 300
+    },
+    {
+      title: "sản phẩm 4",
+      urlImage: "https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg",
+      price: 400
+    }
+
+  ]
+
   return (
     <>
-      <ProductItem
-        title="Sản phẩm 1"
-        urlImage="https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg"
-        price={1000}
-      />
-
-      <ProductItem
-        title="Sản phẩm 2"
-        urlImage="https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg"
-        price={2000}
-      />
-      {/* <ProductItem/>
-      <ProductItem/> */}
+      {
+        listProduct.map((item: ProductType,index: number)=>{
+          return (
+            <ProductItem
+              key={index}
+              title={item.title}
+              urlImage={item.urlImage}
+              price={item.price}
+            />
+          )
+        })
+      }
     </>
   )
 
