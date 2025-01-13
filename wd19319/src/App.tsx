7c5fb35d3a55e6e89d2b45fea09d188e34223b17
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-13 16:43:07 +0700

    component

--- wd19319/src/App.tsx
+++ wd19319/src/App.tsx
@@ -2,19 +2,6 @@
 import MyComponent from "./components/MyComponent";
 
 function App() {
-  const myName: any = "chinhpd5";
-
-  console.log(myName);
-  // console.log(123);
-
-  function test(a: number, b: number): void {
-    // debugger;
-    console.log(a + b);
-  }
-
-  test(1, 2);
-
-  test(3, 4);
 
   return (
     <>
--- wd19319/src/components/MyComponent.tsx
+++ wd19319/src/components/MyComponent.tsx
@@ -1,8 +1,23 @@
 
+function ProductItem(){
+  return(
+    <>
+      <h1>Sản phẩm 1</h1>
+      <img style={{height: '100px'}} src="https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg" alt="" />
+      <h3>1000 vnđ</h3>
+      <button>Mua ngay</button>
+    </>
+  )
+}
+
 function MyComponent(){
-    return (
-        <h1>Xin chào</h1>
-    )
+  return (
+    <>
+      <ProductItem/>
+      <ProductItem/>
+      <ProductItem/>
+    </>
+  )
 }
 
 export default MyComponent;
