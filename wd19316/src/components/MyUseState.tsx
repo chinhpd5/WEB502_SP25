047c967465eb07290c07b0c36f770422feb5724b
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-13 20:01:19 +0700

    callback usestate

--- wd19316/src/components/MyUseState.tsx
+++ wd19316/src/components/MyUseState.tsx
@@ -4,17 +4,25 @@ function MyUseState(){
 
   const [counter,setCounter] = useState<number>(0)
 
-  const handlIncrease = ()=>{
-    setCounter(counter+1)
+  const handleIncrease = ()=>{
+    setCounter((prev) => { // prev giá trị cũ của counter
+      return prev+1
+    })
+    setCounter((prev) => prev+1)
+    setCounter((prev) => prev+1)
   }
 
   return (
     <>
       <h1>UseState</h1>
       <h1>{counter}</h1>
-      <button onClick={handlIncrease}>Tăng</button>
-      <button>Giảm</button>
-      <button>Reset</button>
+      <button onClick={handleIncrease}>Tăng</button>
+      <button onClick={()=>{
+        setCounter(counter-1)
+      }}>Giảm</button>
+      <button onClick={()=>{
+        setCounter(0)
+      }}>Reset</button>
     </>
   )
 }
