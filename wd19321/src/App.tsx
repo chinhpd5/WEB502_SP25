a80a04afbea481141ed79fbef10e319746f61cbc
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-16 08:30:18 +0700

    eslint

--- wd19321/src/App.tsx
+++ wd19321/src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const myName: string = "chinhpd5";
+  
+  function sum(a: number, b: number): void{
+    debugger;
+    console.log(a+b);
+    
+  }
+  sum(3,4);
+
+  console.log(123);
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
