d81db179a15d4201666fe905840ba7f1cc838543
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-18 07:57:35 +0700

    props

--- wd19321/src/components/MyComponent.tsx
+++ wd19321/src/components/MyComponent.tsx
@@ -1,20 +1,43 @@
+// Props trong:
+// React component
+// React element (class -> className, for -> htmlFor)
 
-function Header(){
+type ProductType = {
+  title: string;
+  urlImage: string;
+  price: number;
+};
+
+function ProductItem(props: ProductType) {
+  console.log(props);
+
   return (
-    <h1>Header</h1>
-  )
+    <div>
+      <h1 id="title-heading">{props.title}</h1>
+      <img style={{ height: "100px" }} src={props.urlImage} alt="" />
+      <h4 className="price">{props.price} vnđ</h4>
+      <button>Mua ngay</button>
+    </div>
+  );
 }
 
-
-function MyComponent(){
-
+function MyComponent() {
   return (
     <>
-      <Header/>
-      <h1>Content</h1>
-      <h1>Footer</h1>
+      <h1>Element</h1>
+      <ProductItem
+        title="Sản phẩm 1"
+        urlImage="https://atinproduction.com/wp-content/uploads/2021/07/AWP01220-scaled-1280x1920.jpg"
+        price={1000}
+      />
+
+      <ProductItem
+        title="Sản phẩm 2"
+        urlImage="https://studiovietnam.com/wp-content/uploads/2022/08/background-chup-anh-san-pham-02.jpg"
+        price={2000}
+      />
     </>
-  )
+  );
 }
 
-export default MyComponent
+export default MyComponent;
