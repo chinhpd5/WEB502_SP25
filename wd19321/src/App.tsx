ef77bed4bc99e81eeec259392a9e1a7a589825b6
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-18 07:39:40 +0700

    component

--- wd19321/src/App.tsx
+++ wd19321/src/App.tsx
@@ -1,19 +1,11 @@
 import "./App.css";
+import MyComponent from "./components/MyComponent";
 
 function App() {
-  // const myName: string = "chinhpd5";
-
-  function sum(a: number, b: number): void {
-    // debugger; // hỗ trợ việc debug
-    console.log(a + b);
-  }
-
-  sum(3, 4);
-
-  // console.log(123);
 
   return (
     <>
+      <MyComponent/>
     </>
   );
 }
---
+++ wd19321/src/components/MyComponent.tsx
@@ -0,0 +1,20 @@
+
+function Header(){
+  return (
+    <h1>Header</h1>
+  )
+}
+
+
+function MyComponent(){
+
+  return (
+    <>
+      <Header/>
+      <h1>Content</h1>
+      <h1>Footer</h1>
+    </>
+  )
+}
+
+export default MyComponent
