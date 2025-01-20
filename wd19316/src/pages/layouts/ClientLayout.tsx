c2762cb9b5b815bc60a85a73d9a09f9d7dc06782
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-20 19:47:36 +0700

    navbar

--- wd19316/src/pages/layouts/ClientLayout.tsx
+++ wd19316/src/pages/layouts/ClientLayout.tsx
@@ -1,16 +1,98 @@
-import { Outlet } from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
 
-function ClientLayout(){
-
+function ClientLayout() {
   return (
     <>
-        <h1>Menu layout</h1>
+      <nav className="navbar navbar-expand-lg" style={{backgroundColor: '#f379d0'}}>
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Trang chủ
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/product">
+                  Sản phẩm
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/news">
+                  Tin tức
+                </Link>
+              </li>
+              {/* <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">
+                  Disabled
+                </a>
+              </li> */}
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
 
-        <Outlet/>
+      <Outlet />
 
-        <h1>Footer layout</h1>
+      {/* <h1>Footer layout</h1> */}
     </>
-  )
+  );
 }
 
 export default ClientLayout;
