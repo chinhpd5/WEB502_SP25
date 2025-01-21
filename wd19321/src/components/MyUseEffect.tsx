2b00683b406abaaf10c878a727f9f73629669fad
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-01-21 07:42:35 +0700

    useEffect

--- wd19321/src/components/MyUseEffect.tsx
+++ wd19321/src/components/MyUseEffect.tsx
@@ -6,9 +6,18 @@ type UserType = {
   username: string,
   website: string
 }
+const userInit: UserType ={
+  id:0,
+  name: '',
+  phone:'',
+  username: '',
+  website:''
+}
 
 function MyUseEffect (){
   const [users,setUsers] = useState<UserType[]>([])
+  const [id,setId] = useState<number>(0)
+  const [user,setUser] = useState<UserType>(userInit)
 
   // useEffect(callback): callback chạy lần đầu khi component render, 
   // callback được gọi lại khi component re-render
@@ -49,6 +58,22 @@ function MyUseEffect (){
   },[])
 
   // useEffect(callback,[deps1,deps2])
+  // callback chạy lần đầu tiên khi component render
+  // callback sẽ được gọi lại khi dep có sự thay đổi
+  useEffect(()=>{
+    if(id){
+      const getUserById =async ()=>{
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        const data = await res.json();
+        console.log(data);
+        if(data)
+          setUser(data)
+      }
+
+      getUserById();
+    }
+
+  },[id]) // mỗi lần id thay đổi thì call back sẽ được gọi lại
 
   return (
 
@@ -59,12 +84,20 @@ function MyUseEffect (){
         {
           users.map((item: UserType)=>{
             return (
-              <li key={item.id}>{item.id}. {item.name}|{item.phone}</li>
+              <li onClick={()=>{
+                // console.log(item.id);
+                setId(item.id)
+              }} key={item.id}>{item.id}. {item.name}|{item.phone}</li>
             )
 
           })
         }
       </ul>
+
+      <h1>Thông tin chi tiết User</h1>
+      <h3>Họ và tên: {user.name}</h3>
+      <h5>SĐT: {user.phone}</h5>
+      <h5>Website {user.website}</h5>
     </>
   )
 }
