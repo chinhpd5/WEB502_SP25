28c51aab5f615c98095017397d9e62490c9c6c9a
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-02-05 17:49:46 +0700

    fix lỗi

--- wd19319/src/components/ProductItem.tsx
+++ wd19319/src/components/ProductItem.tsx
@@ -5,20 +5,20 @@ function ProductItem(props: IProduct) {
   // console.log(props);
 
   return (
-     <Link to={`/product/${props.id}`} className="mb-3">
-        <div className="card" style={{ width: "300px" }}>
-          <img style={{width: '90%', height:'400px', objectFit:'cover'}} src={props.thumbnail} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title text-truncate">{props.title}</h5>
-            <p className="card-text text-truncate">
-             {props.description}
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
+    <Link to={`/product/${props.id}`} className="mb-3">
+      <div className="card" style={{ width: "300px" }}>
+        <img style={{width: '90%', height:'400px', objectFit:'cover'}} src={props.thumbnail} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title text-truncate">{props.title}</h5>
+          <p className="card-text text-truncate">
+            {props.description}
+          </p>
+          <button className="btn btn-primary">
+            Go somewhere
+          </button>
         </div>
-      </Link>
+      </div>
+    </Link>
   );
 }
 
