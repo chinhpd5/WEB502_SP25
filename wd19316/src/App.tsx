f113442d872df8d73d4f50f5e2eb6de755babbd5
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-02-07 18:50:54 +0700

    route admin

--- wd19316/src/App.tsx
+++ wd19316/src/App.tsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ClientLayout from "./pages/layouts/ClientLayout";
 import ProductDetail from "./pages/ProductDetail";
+import AdminLayout from "./pages/layouts/AdminLayout";
+import List from "./pages/products/List";
+import Add from "./pages/products/Add";
 
 function App() {
 
@@ -17,6 +20,11 @@ function App() {
           <Route path="product/:id" element={<ProductDetail/>} />
           <Route path="about" element={<h1>About</h1>} />
           <Route path="news" element={<h1>Tin tức</h1>} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout/>}>
+          <Route path="product" element={<List/>}/>
+          <Route path="product/add" element={<Add/>}/>
         </Route>
 
         <Route path="*" element={<h1>Not found</h1>}/>
