b2d1d1bbe7edef1a4119fa39de2f81da7528c45d
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-02-08 07:56:40 +0700

    giao diện thêm mới

--- wd19321/src/pages/products/Add.tsx
+++ wd19321/src/pages/products/Add.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
 
 function Add() {
   return (
-    <div>Add</div>
-  )
+    <div>
+      <h1>Thêm mới sản phẩm</h1>
+
+      <form>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Tên sản phẩm
+          </label>
+          <input type="text" className="form-control" id="title" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">
+            Giá bán
+          </label>
+          <input type="number" className="form-control" id="price" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="thumbnail" className="form-label">
+            Hình ảnh
+          </label>
+          <input type="text" className="form-control" id="thumbnail" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Mô tả
+          </label>
+          <input type="text" className="form-control" id="description" />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="category" className="form-label">
+            Danh mục
+          </label>
+          <select className="form-select" >
+            <option value={'smartphones'}>smartphones</option>
+            <option value={'laptops'}>laptops</option>
+            <option value={'skincare'}>skincare</option>
+          </select>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default Add
+export default Add;
